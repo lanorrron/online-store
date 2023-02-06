a28fc0c33ce1fe8e98c34a8fd93a1483cdc487ad
lanorrron <lanorr.ron@gmail.com>
2023-02-06 09:58:59 -0400

    Create view of categoryList

---
+++ resources/views/livewire/category/category-list.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10 border">
+                <div class="flex justify-between mb-5">
+                    <h1 class="font-bold text-2xl">{{'CATEGORÍAS'}}</h1>
+                    <button class="btn-green">{{'Nuevo'}}</button>
+                </div>
+                <div class="overflow-hidden rounded-lg border border-gray-200 shadow-md">
+                    @if(!empty($categories))
+                        <table class="w-full border-collapse bg-white text-left text-base text-gray-500 table-fixed">
+                            <thead>
+                            <tr>
+                                <th>{{'ID'}}</th>
+                                <th>{{'Nombre'}}</th>
+                                <th>{{'Acciones'}}</th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+                            @foreach($categories as $category)
+                                <tr>
+                                    <td>{{$category->id}}</td>
+                                    <td>{{$category->name}}</td>
+                                    <td class="flex gap-5">
+                                        <x-edit-button></x-edit-button>
+                                        <x-delete-button></x-delete-button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    @else
+                        <p class="text-center">{{'No hay datos a mostrar'}}</p>
+                    @endif
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
